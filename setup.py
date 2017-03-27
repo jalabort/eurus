37f70196185f4daa3d9ff61ff81be9193734481e
Joan Alabort <joan.alabort@hudl.com>
2017-03-27 10:57:36 +0100

    Update dockerfiles

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import os
 from setuptools import setup, find_packages
 
 
 setup(
     name='eurus',
     version='0.0.0',
-    packages=find_packages()
+    packages=find_packages(),
+    scripts=[[os.path.join(root, f) for f in files]
+             for root, _, files in
+             os.walk(os.path.join('eurus', 'cli', 'bin'))][0],
 )
