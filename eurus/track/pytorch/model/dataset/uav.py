266fc341899ebd972cd3bf6583882414661b44de
Joan Alabort <joan.alabort@hudl.com>
2017-03-30 11:53:30 +0100

    Add dataset sequence statistics

--- eurus/track/pytorch/model/dataset/uav.py
+++ eurus/track/pytorch/model/dataset/uav.py
@@ -1,11 +1,13 @@
-import csv
 import os
+import csv
+import numbers
 
 import numpy as np
 
 from PIL import Image
 from torch.utils.data import Dataset
 
+from .utils import crop, display_1d_histogram
 from .widgets.exception import IPythonWidgetsMissingError
 
 
@@ -34,10 +36,34 @@ class Uav123(Dataset):
     ----------
     M. Mueller, et al. "A Benchmark and Simulator for UAV Tracking". ECCV 2016.
     """
-    def __init__(self, root, transform=None, target_transform=None):
+    def __init__(self, root, transform=None, target_transform=None,
+                 sequence_length=10, skip=25, context_factor=3,
+                 search_factor=2, context_size=128, search_size=256):
+
         self.root = root
         self.transform = transform
         self.target_transform = target_transform
+        self.sequence_length = sequence_length
+        self.skip = skip
+
+        self.context_factor = context_factor
+        self.search_factor = search_factor
+
+        if isinstance(context_size, numbers.Number):
+            self.context_size = np.array([context_size, context_size]).astype(
+                np.int32)
+        elif isinstance(context_size, tuple):
+            self.context_size = np.array(context_size)
+        else:
+            raise ValueError('`context_size` must be `int` or `(int, int)`.')
+
+        if isinstance(search_size, numbers.Number):
+            self.search_size = np.array([search_size, search_size]).astype(
+                np.int32)
+        elif isinstance(search_size, tuple):
+            self.search_size = np.array(search_size).astype(np.int32)
+        else:
+            raise ValueError('`context_size` must be `int` or `(int, int)`.')
 
         img_path = os.path.join(root, 'data_seq', 'UAV123')
         ann_path = os.path.join(root, 'anno', 'UAV123')
@@ -78,21 +104,41 @@ class Uav123(Dataset):
                 'in sequences {} should be the same.'.format(
                     len(img_list2), len(ann_list2), i)
 
+    @property
+    def _n_elements_per_sequence(self):
+        return [len(sequence) for sequence in self.img_list]
+
+    @property
+    def n_shortest(self):
+        return np.min(self._n_elements_per_sequence)
+
+    @property
+    def n_longest(self):
+        return np.max(self._n_elements_per_sequence)
+
+    @property
+    def n_average(self):
+        return np.mean(self._n_elements_per_sequence)
+
+    @property
+    def n_median(self):
+        return np.median(self._n_elements_per_sequence)
+
     def __len__(self):
         return len(self.img_list)
 
-    def _get_item(self, index):
+    def _get_sequence_from_index(self, index):
         r"""
-        Get an item from the dataset.
+
 
         Parameters
         ----------
-        index : int
+        index :
 
 
         Returns
         -------
-        items: (list[np.ndarray], list[np.ndarray])
+        sequences : (list[np.ndarray], list[np.ndarray])
 
         """
         if index < 0 or index > len(self):
@@ -103,27 +149,97 @@ class Uav123(Dataset):
         img_sequence = self.img_list[index]
         ann_sequence = self.ann_list[index]
 
-        new_img_sequence = [Image.open(img_path) for img_path in img_sequence]
+        first = np.random.randint(
+            0, len(img_sequence) - self.sequence_length * self.skip)
+        last = first + self.sequence_length * self.skip
 
-        return new_img_sequence, ann_sequence
+        return (img_sequence[first:last:self.skip],
+                ann_sequence[first:last:self.skip])
 
     def __getitem__(self, index):
-        img_sequence, ann_sequence = self._get_item(index)
+        img_sequence, ann_sequence = self._get_sequence_from_index(index)
 
-        if self.transform is not None:
-            img_sequence = [self.transform(img) for img in img_sequence]
+        for i in range(len(img_sequence) - 1):
+            img1 = Image.open(img_sequence[i])
+            if img1.mode == 'L':
+                img1 = img1.convert(mode='RGB')
+            img1_box = ann_sequence[i]
 
-        if self.target_transform is not None:
-            ann_sequence = [self.target_transform(ann) for ann in ann_sequence]
+            img1_tl = img1_box[:2]
+            img1_sz = img1_box[2:]
+            img1_center = img1_tl + img1_sz / 2
 
-        return img_sequence, ann_sequence
+            max_sz = max(img1_sz)
+            img1_crop_size = np.array([max_sz, max_sz]) * self.context_factor
 
-    def view(self):
+            img1_center += np.random.randn(2) * 0.01 * img1_crop_size
+            img1_crop_size += np.random.randn(2) * 0.05 * img1_crop_size
+
+            context = crop(img1, img1_center, img1_crop_size)
+            context = context.resize(self.context_size, resample=Image.BICUBIC)
+
+            ratio1 = img1_crop_size / self.context_size
+            corrector = img1_center - img1_crop_size / 2
+            context_tl = (img1_box[:2] - corrector) / ratio1
+            context_sz = img1_box[2:] / ratio1
+            context_box = np.concatenate([context_tl, context_sz])
+
+            img2 = Image.open(img_sequence[i + 1])
+            if img2.mode == 'L':
+                img2 = img1.convert(mode='RGB')
+            img2_box = ann_sequence[i + 1]
+
+            img2_crop_size = img1_crop_size * self.search_factor
+
+            search = crop(img2, img1_center, img2_crop_size)
+            search = search.resize(self.search_size, resample=Image.BICUBIC)
+
+            ratio2 = img2_crop_size / self.search_size
+
+            corrector = img1_center - img2_crop_size / 2
+            search_tl = (img2_box[:2] - corrector) / ratio2
+            search_sz = img2_box[2:] / ratio2
+            search_box = np.concatenate([search_tl, search_sz])
+
+            aux_tl = (img1_box[:2] - corrector) / ratio2
+            aux_sz = img1_box[2:] / ratio2
+            search_context_box = np.concatenate([aux_tl, aux_sz])
+
+            if self.transform is not None:
+                context = self.transform(context)
+                search = self.transform(search)
+            if self.target_transform is not None:
+                context_box = self.target_transform(context_box)
+                search_box = self.target_transform(search_box)
+
+            yield context, search, context_box, search_box, search_context_box
+
+    def view_original(self):
         r"""
-        Visualize dataset.
+        Visualize the original unprocessed dataset.
         """
         try:
             from .widgets import notebook_view_sequence
             notebook_view_sequence(self.img_list, self.ann_list)
         except ImportError:
             raise IPythonWidgetsMissingError()
+
+    def view_sequence_length_histogram(self):
+        r"""
+        Visualize the histogram of sequence lengths.
+        """
+        display_1d_histogram(self._n_elements_per_sequence)
+
+    def __str__(self):
+        return ('{} dataset:\n'
+                '  - # of sequences: {}\n'
+                '    - Shortest sequence length: {}\n'
+                '    - Longest sequence length:  {}\n'
+                '    - Average sequence length:  {}\n'
+                '    - Median sequence length:   {}\n'
+                .format(self.__class__.__name__,
+                        len(self),
+                        self.n_shortest,
+                        self.n_longest,
+                        self.n_average,
+                        self.n_median))
--- eurus/track/pytorch/model/dataset/utils.py
+++ eurus/track/pytorch/model/dataset/utils.py
@@ -1,8 +1,34 @@
+import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 
-def display_image(img, ann, figure_size=(16, 9), color='red', fill=False,
+def crop(img, center, size):
+    r"""
+
+
+    Parameters
+    ----------
+    img : np.ndarray
+        The image to be cropped.
+    center: np.ndarray
+
+    size:
+
+    Returns
+    -------
+    cropped_img: np.ndarray
+        The cropped image.
+    """
+    x, y = center
+    w, h = size
+    x1 = int(round(x - w / 2.))
+    y1 = int(round(y - h / 2.))
+    return img.crop((x1, y1, x1 + w, y1 + h))
+
+
+def display_image(img, box, figure_size=(16, 9), color='red', fill=False,
                   alpha=1.0):
     r"""
     Display image with overlaid bounding box.
@@ -11,7 +37,7 @@ def display_image(img, ann, figure_size=(16, 9), color='red', fill=False,
     ----------
     img : np.ndarray
         ``(height, width, 3)`` array containing the image to be displayed.
-    ann : np.ndarray
+    box : np.ndarray
         ``(4, )`` array containing the top left vertex and the size of the
         bounding box to be displayed.
     figure_size : tuple(int), optional
@@ -26,7 +52,65 @@ def display_image(img, ann, figure_size=(16, 9), color='red', fill=False,
     fig, ax = plt.subplots(1, figsize=figure_size)
     ax.set_axis_off()
     ax.imshow(img)
-    box = patches.Rectangle((ann[0], ann[1]), ann[2], ann[3],
+    box = patches.Rectangle((box[0], box[1]), box[2], box[3],
                             color=color, fill=fill, alpha=alpha)
     ax.add_patch(box)
+    plt.show()
+
+
+def display_search_image(img, box, context_box, figure_size=(16, 9),
+                         color_box='red', color_context='blue', fill=False,
+                         alpha=1.0):
+    r"""
+    Display image with overlaid bounding box.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        ``(height, width, 3)`` array containing the image to be displayed.
+    box : np.ndarray
+        ``(4, )`` array containing the top left vertex and the size of the
+        bounding box to be displayed.
+    context_box : np.ndarray
+
+    figure_size : tuple(int), optional
+        ``(2,)`` tuple defining the figure size.
+    color_box : str, optional
+        The color of the bounding box.
+    color_context : str, optional
+
+    fill : bool, optional
+        Whether to display a filled bounding box or not.
+    alpha: float, optional
+        The value of the alpha channel of the bounding box.
+    """
+    fig, ax = plt.subplots(1, figsize=figure_size)
+    ax.set_axis_off()
+    ax.imshow(img)
+    ax.scatter(int(img.size[1] / 2), int(img.size[0] / 2), marker='x')
+    box = patches.Rectangle((box[0], box[1]), box[2], box[3],
+                            color=color_box, fill=fill, alpha=alpha)
+    ax.add_patch(box)
+    box = patches.Rectangle((context_box[0], context_box[1]),
+                            context_box[2], context_box[3],
+                            color=color_context, fill=fill, alpha=alpha)
+    ax.add_patch(box)
+    plt.show()
+
+
+# TODO: Expose more matplotlib histogram options
+def display_1d_histogram(data, figure_size=(16, 9)):
+    r"""
+    Display the histogram of the given data.
+    
+    Parameters
+    ----------
+    data : list | np.ndarray
+       ``(n,)`` list or array containing the data for which the histogram 
+       needs to be generated.
+    figure_size : tuple(int), optional
+        ``(2,)`` tuple defining the figure size.
+    """
+    fig, ax = plt.subplots(1, figsize=figure_size)
+    ax.hist(data, 'auto')
     plt.show()
